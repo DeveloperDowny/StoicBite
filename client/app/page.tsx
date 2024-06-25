5d8a2bfd60ab964c245d9bfd127fc7a5c3c5d780
DeveloperDowny <vedantpanchal12345@gmail.com>
2024-06-26 03:03:52 +0530

    fixing jank

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,19 +2,30 @@
 
 import QuoteCont from "@/components/QuoteCont/QuoteCont";
 import QuoteExplain from "@/components/QuoteExplain/QuoteExplain";
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+import gsap from "gsap";
 
 const QuotePage = () => {
+  const parentContRef = useRef(null);
   // when page loads set window.innerHeight to css variable --full_height
-  React.useEffect(() => {
+  useEffect(() => {
     document.documentElement.style.setProperty(
       "--full_height",
       `${window.innerHeight}px`
     );
   }, []);
-  
+
+  // take 2 sec to fade in the parent container
+  useEffect(() => {
+    gsap.to(parentContRef.current, {
+      duration: 2,
+      opacity: 1,
+    });
+  }, []);
+
   return (
-    <div className="max-w-[28rem]">
+    <div className="max-w-[28rem] opacity-0" ref={parentContRef}>
       <QuoteCont />
       <QuoteExplain />
     </div>
