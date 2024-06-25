8585e686900b7f45adf91f0dcedc26f9ce07cabf
DeveloperDowny <vedantpanchal12345@gmail.com>
2024-06-26 02:52:08 +0530

    fixv1 for full screen window.innerHeigh

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import QuoteCont from "@/components/QuoteCont/QuoteCont";
 import QuoteExplain from "@/components/QuoteExplain/QuoteExplain";
 import React from "react";
 
 const QuotePage = () => {
+  // when page loads set window.innerHeight to css variable --full_height
+  React.useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--full_height",
+      `${window.innerHeight}px`
+    );
+  }, []);
+  
   return (
     <div className="max-w-[28rem]">
       <QuoteCont />
